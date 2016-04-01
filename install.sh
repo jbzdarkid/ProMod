7942dfeeb11b470c9b0286218982b934f0aacdfe
jbzdarkid <jbzdarkid@gmail.com>
2016-04-01 08:39:44 -0400

    Exit if install fails.

--- install.sh
+++ install.sh
@@ -52,6 +52,10 @@ while [ ! -d "server01" && $COUNT -lt 10 ]; do
   let COUNT=COUNT+1
   ./steamcmd.sh +login anonymous +force_install_dir ./server01 +app_update 222860 validate +quit
 done
+if [ ! -d "server01" ] then # Install failed
+  echo "L4D2 Server install failed!"
+  exit -1
+fi
 
 # Download and install ProMod
 cd ~/
