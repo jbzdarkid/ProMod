702dd8deb2f32c90c137854f11c8284bd15a3b7f
jbzdarkid <jbzdarkid@gmail.com>
2016-04-03 09:56:25 -0400

    Leave the directory so the rest of the script doesn't break
    (getting lost part 2)

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ download "https://www.sourcemod.net/smdrop/1.6/sourcemod-1.6.0-git4525-linux.tar
 download "http://www.bailopan.net/stripper/files/stripper-1.2.2-linux.tar.gz" "stripper.tar.gz" && tar -xvzf "stripper.tar.gz"
 download "http://users.alliedmods.net/~drifter/builds/dhooks/2.0/dhooks-2.0.4-hg82-linux.tar.gz" "dhooks2.tar.gz" && tar -xvzf "dhooks2.tar.gz"
 # Zipballs
-download "https://api.github.com/repos/jbzdarkid/ProMod/zipball" "promod.zip" && unzip "promod.zip" && cd "$(ls | grep jbzdarkid)" && chmod +x build.sh && ./build.sh && chmod +x server.sh && mv server.sh ~/server01 && cp -r * ../addons/sourcemod/
+download "https://api.github.com/repos/jbzdarkid/ProMod/zipball" "promod.zip" && unzip "promod.zip" && cd "$(ls | grep jbzdarkid)" && chmod +x build.sh && ./build.sh && chmod +x server.sh && mv server.sh ~/server01 && cp -r * ../addons/sourcemod/ && cd ..
 download "https://forums.alliedmods.net/attachment.php?attachmentid=83286?attachmentid=83286" "socket.zip" && unzip "socket.zip"
 download "https://forums.alliedmods.net/attachment.php?attachmentid=122230&d=1373147952" "l4dtoolz.zip" && unzip "l4dtoolz.zip" -d addons/
 download "https://forums.alliedmods.net/attachment.php?attachmentid=143904&d=142830828"4 "GeoIPCity.zip" && unzip "GeoIPCity.zip" -d addons/sourcemod/
